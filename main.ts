a2fa6a4c1b96000b3b785563a0a34c87d366d211
Carl0s-Eduard0 <129457814+Carl0s-Eduard0@users.noreply.github.com>
2024-09-25 20:36:52 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,14 @@ import { Controle } from './control';
 
 let controle = new Controle();
 
-controle.setVelocidadePermitida(120); // define a velocidade permitida
-
-controle.incrementarVelocidade(10); // incrementa a velocidade em 10
-console.log(controle.getVelocidade()); // printa a velocidade atual
-
-controle.incrementarVelocidade(20); // incrementa a velocidade em 20
-console.log(controle.getVelocidade()); // printa a velocidade atual
+controle.setVelocidadePermitida(120); 
+controle.incrementarVelocidade(10); 
+console.log(controle.getVelocidade()); 
+controle.incrementarVelocidade(20); 
+console.log(controle.getVelocidade()); 
 
 try {
-  controle.incrementarVelocidade(100); // incrementa a velocidade em 100, estourando o limite permitido
+  controle.incrementarVelocidade(100); 
 } catch (error) {
-  console.error(error); // printa o erro
+  console.error(error); 
 }
